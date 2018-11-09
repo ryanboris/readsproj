3c5ee35bd731687a7921d2e76d2046c3f0f5ae14
ryanboris <ryan.boris@gmail.com>
2018-11-09 18:10:56 -0500

    Feat: Starts to break the disorganized highly repetitive HTML into components

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,38 +1,33 @@
 import React from 'react';
+import { search, get } from './BooksAPI';
 
 class SearchPage extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { query: '' };
+        this.state = {};
     }
+    searchBooks = () => {
+        search(this.state.text).then(res => get(res.id));
+    };
+
     render() {
         return (
             <div className="search-books">
+                {}{' '}
                 <div className="search-books-bar">
                     <a className="close-search" onClick={() => this.props.handleState}>
                         Close
                     </a>
                     <div className="search-books-input-wrapper">
-                        <form
-                            onSubmit={() => {
-                                this.props.searchBooks(this.state.type);
-                            }}
-                        >
+                        <form onSubmit={this.searchBooks}>
                             <input
                                 type="text"
                                 placeholder="Search by title or author"
-                                onChange={e => {
-                                    this.setState({
-                                      [e.target.type]: e.target.value
-                                    });
-                                }}
+                                onChange={e => this.setState({ [e.target.type]: e.target.value })}
                             />
-                            <input type="submit" value="submit" />
+                            <input type="submit" value="+" />
                         </form>
                     </div>
-                </div>
-                <div className="search-books-results">
-                    <ol className="books-grid" />
                 </div>
             </div>
         );
